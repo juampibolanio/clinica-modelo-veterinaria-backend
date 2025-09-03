78d4fd4ef7da37497619cd9b88a2a18b4bbd09ca
JuampiBolanio <juampi360786@gmail.com>
2025-09-03 13:57:24 -0300

    Commit inicial - Backend - Clínica Veterinaria

---
+++ src/main/java/com/cmv/vetclinic/VetClinicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cmv.vetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetClinicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetClinicBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmv/vetclinic/VetClinicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmv.vetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetClinicBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
